52bee2392788de45229ee80ef0ca148f88a67b98
EduardoJCM <98174532+EduardoJCM@users.noreply.github.com>
2024-03-03 00:00:16 -0600

    Add files via upload

---
+++ Practica 2 Codigo Visual/Practica22/default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practica22
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ClsDaoLibros ObjLibros = new ClsDaoLibros();
+            DataTable datos = ObjLibros.CargarDatosGridView();
+            GridView1.DataSource = datos;
+            GridView1.DataBind();
+        }
+
+        protected void BtnAdd_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("frmAdd.aspx");
+        }
+    }
+}
